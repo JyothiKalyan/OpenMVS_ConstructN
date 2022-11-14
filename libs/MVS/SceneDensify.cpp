f8d33671aefb82f159aa1cbc8f0f260b8f815dfd
JyothiKalyan <96404574+JyothiKalyan@users.noreply.github.com>
2022-11-14 13:18:21 +0530

    data.images added

--- libs/MVS/SceneDensify.cpp
+++ libs/MVS/SceneDensify.cpp
@@ -1928,14 +1928,13 @@ for (IIndex idx: data.images)
 	}
 
 	const Image8U::Size sizeMap(depthData_loaded.depthMap.size());
-	//sizeMap.height; sizeMap.width;
-	 for (int i=0; i<10; ++i) {
-		for (int j=0; j<10 ;++j) {
+	//; ;
+	 for (int i=0; i<sizeMap.height; ++i) {
+		for (int j=0; j<sizeMap.width; ++j) {
+			//depthData.depthMap(r, c) = depth;
 			std::cout<<i<<"***"<<j<<"***"<<depthData_loaded.depthMap(i,j)<<" ";
 	}}	
-				
-
-	//depthData_loaded.Save(ComposeDepthFilePath(depthData.GetView().GetID(), data.nEstimationGeometricIter < 0 ? "dmap" : "geo.dmap"));
+	depthData_loaded.Save(ComposeDepthFilePath(depthData.GetView().GetID(), data.nEstimationGeometricIter < 0 ? "dmap" : "geo.dmap"));
 	std::cout<<"\nsaved!!!!!!"<<rawName<<"\n";
 	
 }
